814331684038985812305787218497b42df97fa2
sixfwa <farhanaliw6@gmail.com>
2019-12-11 16:30:40 +0100

    particle position between -2 and 2

--- coursework/PSO/particle.py
+++ coursework/PSO/particle.py
@@ -1,10 +1,11 @@
 import numpy as np
+import random
 
 
 class Particle:
 
     def __init__(self):
-        pos = np.random.rand(13)
+        pos = self.initialise_position()
         vel = np.random.rand(13)
         self.position = list(pos)
         self.velocity = list(abs(pos - vel) / 2)
@@ -13,3 +14,10 @@ class Particle:
     def set_personal_best(self):
         self.personal_best_position = self.position
         self.personal_best_value = self.position_value
+
+    def initialise_position(self):
+        pos = []
+        for i in range(13):
+            pos.append(random.uniform(-2, 2))
+
+        return pos
